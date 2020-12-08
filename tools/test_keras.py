0a7badc84698492966867e8e743c1cdaf63e800f
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2020-12-08 16:13:58 +0000

    Make keras prediction for 1 heat map

--- tools/test_keras.py
+++ tools/test_keras.py
@@ -1,5 +1,15 @@
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+from lib.config import config, update_config
+from lib.datasets import get_dataset
+from lib.core.evaluation import decode_preds
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 from tensorflow import keras
+from tensorflow.keras.applications.resnet import ResNet50, preprocess_input
 
 class Object(object):
     pass
@@ -25,7 +35,19 @@ if __name__=="__main__":
 		img2 = preprocess_input(np.dstack((imgPlanes[0,:,:], imgPlanes[1,:,:], imgPlanes[2,:,:])))
 		X_test.append(img2)
 
+	X_test = np.array(X_test)
+	print (X_test.shape)
+
 	pred = model.predict(X_test)
 
 	print (pred.shape)
+
+	for i in range(pred.shape[0]):
+		img = pred[i, :]
+		img = img[:45*45].reshape((45, 45))
+		plt.imshow(img)
+		plt.show()
+
+	#preds = decode_preds(score_map, meta['center'], meta['scale'], [64, 64])
+
 
